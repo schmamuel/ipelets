8f8a536383f31e89180adf65cdcdbfdde6cc2793
Samuel <40206193+Kenderi@users.noreply.github.com>
2025-05-08 14:19:13 +0200

    Update set_minipage.lua

--- set_minipage.lua
+++ set_minipage.lua
@@ -39,6 +39,32 @@ function check_available_styles(model)
 end
 
 
+local function style_names(model)
+    local sheets = model.doc:sheets()
+    return sheets:allNames("textstyle")
+end
+
+local function mainWindow(model)
+    if model.ui.win == nil then
+       return model.ui
+    else
+       return model.ui:win()
+    end
+ end
+
+local function ask_for_style(model)
+    local dialog = ipeui.Dialog(mainWindow(model), "Select a style.")
+    local styles = style_names(model)
+    dialog:add("sty", "combo", styles, 1, 1, 1, 2)
+    dialog:add("ok", "button", { label="&Ok", action="accept" }, 2, 2)
+    dialog:add("cancel", "button", { label="&Cancel", action="reject" }, 2, 1)
+    local r = dialog:execute()
+    if not r then return end
+    local style_name = styles[dialog:get("sty")]
+    return style_name
+ end
+
+
 function set_minipage(model,num)
 	local p = model:page()
 	local selection = model:selection()
@@ -80,35 +106,59 @@ function set_minipage(model,num)
     model:register(t) 
 end
 
-function set_item(model)
-    check_available_styles(model)
-	local p = model:page()
-	local selection = model:selection()
-    local t = { 
-            label = "set to next style",
-		   pno = model.pno,
-		   selection = selection,
-           bool = bool,
-           neg = neg,
-           original = p:clone(),
-           undo = _G.revertOriginal
-		 }
+-- function set_item(model)
+--     check_available_styles(model)
+-- 	local p = model:page()
+-- 	local selection = model:selection()
+--     local t = { 
+--             label = "set to next style",
+-- 		   pno = model.pno,
+-- 		   selection = selection,
+--            bool = bool,
+--            neg = neg,
+--            original = p:clone(),
+--            undo = _G.revertOriginal
+-- 		 }
 
+--     t.redo = function (t, doc)
+--         local p = doc[t.pno]
+--         for _,i in ipairs(selection) do
+--             if p[i]:type() == "text" then
+--                 style = get_next_style(p[i]:get("textstyle"))
+--                 if style == nil then
+--                     model:warning("Style was not found.")
+--                 else
+--                     p[i]:set("textstyle", style)
+--                 end
+--             end
+--         end
+--     end
+--     model:register(t) 
+-- end
+
+local function set_item(model)
+    local p = model:page()
+    local style_name = ask_for_style(model)
+    if (not style_name) then return end
+    local selection = model:selection()
+
+    local t = { 
+        label = "changed style",
+        pno = model.pno,
+       selection = selection,
+       style_name = style_name,
+       original = p:clone(),
+       undo = _G.revertOriginal
+     }
     t.redo = function (t, doc)
-        local p = doc[t.pno]
         for _,i in ipairs(selection) do
             if p[i]:type() == "text" then
-                style = get_next_style(p[i]:get("textstyle"))
-                if style == nil then
-                    model:warning("Style was not found.")
-                else
-                    p[i]:set("textstyle", style)
-                end
+                    p[i]:set("textstyle", style_name)
             end
         end
     end
     model:register(t) 
-end
+ end
 
 methods = {
     { label = "change labels to minipages", run = set_minipage},
