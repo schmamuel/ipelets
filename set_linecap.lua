3fc6b43c7339d998b61d2a56aab3e0994f67794b
Samuel <40206193+Kenderi@users.noreply.github.com>
2025-05-16 11:53:59 +0200

    Create set_linecap.lua

---
+++ set_linecap.lua
@@ -0,0 +1,48 @@
+ 
+label = "Linecap" 
+
+about = [[ Provides a shortcut to toggle the linecap of paths between "normal" and "round". ]]
+
+local function set_linecap(model, num)
+    local p = model:page()
+    local selection = model:selection()
+    local t = { 
+        label = methods[num].label,
+        pno = model.pno,
+       selection = selection,
+       style_name = style_name,
+       original = p:clone(),
+       undo = _G.revertOriginal
+     }
+    t.redo = function (t, doc)
+        for _,i in ipairs(selection) do
+            if p[i]:type() == "path" then
+                if num ==1 then
+                    toggle_linecap(p[i])
+                else
+                    p[i]:set("linecap", methods[num].linecap)
+                end
+            end
+        end
+    end
+    model:register(t) 
+ end
+
+ function toggle_linecap(obj)
+    if obj:get("linecap") == "normal" then
+        obj:set("linecap", "round")
+    else
+        obj:set("linecap", "normal")
+
+    end
+ end
+
+methods = {
+    { label = "toggle linecap", run = set_linecap},
+    { label = "set linecap round", run = set_linecap, linecap = "round"},
+    { label = "set linecap normal", run = set_linecap, linecap = "normal"},
+  }
+
+------
+
+shortcuts.ipelet_1_set_linecap = "Alt+Shift+L"
