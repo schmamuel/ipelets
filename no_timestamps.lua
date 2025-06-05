517b0aa16abceaad7f29f74686898cbc1e516703
Samuel <40206193+Kenderi@users.noreply.github.com>
2025-06-05 11:09:11 +0200

    Create no_timestamps.lua

---
+++ no_timestamps.lua
@@ -0,0 +1,81 @@
+
+label = "Save without timestamp"
+
+about = [[ Saves the document without updating the timestamp to make merging easier. ]]
+
+auto_export_directory = "./"
+
+function save(model)
+  local fname = model.file_name
+
+  if not fname then
+    model:action_save_as()
+    return
+  end
+  
+  local fm = _G.formatFromFileName(fname)
+  if not fm then
+    model:warning("File not saved!",
+		 "You must save as *.xml, *.ipe, or *.pdf")
+    return
+  end
+
+  -- run Latex if format is not XML
+  if fm ~= "xml" and not model:runLatex() then
+    model.ui:explain("Latex error - file not saved")
+    return
+  end
+
+  if not model.doc:save(fname, fm) then
+    model:warning("File not saved!", "Error saving the document")
+    return
+  end
+
+  if fm == "xml" and #prefs.auto_export > 0 then
+    model:auto_export(fname)
+  end
+
+  model:markAsUnmodified()
+  model.ui:explain("Saved document '" .. fname .. "'")
+  model.file_name = fname
+  model:setCaption()
+  model:updateRecentFiles(fname)
+  return true
+end 
+
+function change_auto_export_directory(model) 
+  auto_export_directory = model:getString("Enter directory for auto export")
+  model.ui:explain("set auto export directory to " .. auto_export_directory)
+end
+
+methods = {
+   { label = "Save", run=save },
+   { label = "Change auto export directory", run=change_auto_export_directory },
+}
+
+
+--------------
+
+shortcuts.ipelet_1_no_timestamps = shortcuts.save
+shortcuts.save = nil
+
+
+function _G.MODEL:auto_export(fname)
+  if not self:runLatex() then
+    self.ui:explain("Latex error - could not auto-export!")
+  else
+    for _, format  in ipairs(prefs.auto_export) do
+      local ename = fname:sub(1,-4) .. format
+      ename = auto_export_directory .. string.match(ename, "([^/]+)$")
+      if format == "pdf" then
+	      if not self.doc:save(ename, "pdf") then
+	        self:warning("Auto-exporting failed",
+		       "I could not export in PDF format to file '" .. ename .. "'.")
+	      end
+      else
+	      self.ui:renderPage(self.doc, 1, 1,
+			  format, ename, prefs.auto_export_resolution / 72.0,true, false) -- transparent, nocrop
+      end
+    end
+  end
+end
